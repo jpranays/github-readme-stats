ee5481a067a6c01e2a9da7ca7de219946ef26ed2
jpranays <pranay1315@gmail.com>
2025-06-19 22:22:18 +0530

    fix: restore ONE_DAY constant to correct value of 86400 seconds

--- api/pin.js
+++ api/pin.js
@@ -74,10 +74,10 @@ export default async (req, res) => {
       ? parseInt(process.env.CACHE_SECONDS, 10) || cacheSeconds
       : cacheSeconds;
 
-    // res.setHeader(
-    //   "Cache-Control",
-    //   `max-age=${cacheSeconds}, s-maxage=${cacheSeconds}`,
-    // );
+    res.setHeader(
+      "Cache-Control",
+      `max-age=${cacheSeconds}, s-maxage=${cacheSeconds}`,
+    );
 
     return res.send(
       renderRepoCard(
