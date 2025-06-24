3f4997b88e4a93c823025098342e54aa52999167
jpranays <pranay1315@gmail.com>
2025-06-24 10:37:21 +0530

    chore: remove unused render-html endpoint and associated code

--- express.js
+++ express.js
@@ -5,7 +5,6 @@ import langCard from "./api/top-langs.js";
 import wakatimeCard from "./api/wakatime.js";
 import gistCard from "./api/gist.js";
 import express from "express";
-import renderHtml from "./api/render-html.js";
 
 const app = express();
 app.listen(process.env.port || 9000, () => {
@@ -21,4 +20,3 @@ app.get("/pin", repoCard);
 app.get("/top-langs", langCard);
 app.get("/wakatime", wakatimeCard);
 app.get("/gist", gistCard);
-app.get("/render-html", renderHtml);
